30dd1b66db655770769d5661e31239500d350345
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2023-12-08 02:20:06 -0500

    header y footer terminado

---
+++ vista/usuario/footer.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <title></title>
+</head>
+<body>
+<footer class="footer">
+    <div class="footer-section social-media">
+        <h3>Redes Sociales</h3>
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/220px-2021_Facebook_icon.svg.png" alt="Facebook"></a>
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/292px-Logo_of_Twitter.svg.png" alt="Twitter"></a>
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png" alt="Instagram"></a>
+
+    </div>
+
+    <div class="footer-section contact">
+        <h3>Contacto</h3>
+        <p>Email: glamgrid@gg.com</p>
+        <p>Teléfono: +507 233 1458</p>
+
+    </div>
+
+    <div class="footer-section collaborators">
+        <h3>Colaboradores</h3>
+        <ul>
+            <li>Joel Aparicio</li>
+            <li>Daniel Newman</li>
+            <li>Ameth Torrero</li>
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
---
+++ vista/usuario/header.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <title></title>
+</head>
+<body>
+<header class="header">
+    <div class="logo-container">
+        <img src="../img/logo.png" alt="GlamGrid Logo">
+    </div>
+
+    <div class="category-container">
+        <button id="categoryButton">Categorias de ropa</button>
+        <div class="category-dropdown-menu" id="categoryMenu">
+            <a href="#">Camisas</a>
+            <a href="#">Pantalones</a>
+            <a href="#">Zapatos</a>
+            <a href="#">Camisetas</a>
+        </div>
+    </div>
+
+    <div class="user-cart">
+        <div class="user-panel-container">
+            <img src="../img/usuario.png" alt="Panel de Usuario" id="userPanel">
+            <div class="user-dropdown-menu" id="userMenu">
+                <a href="#">Iniciar sesión</a>
+                <a href="#">Registrarse</a>
+            </div>
+        </div>
+        <div class="cart-container">
+            <img src="../img/carrito.png" alt="Carrito" id="shoppingCart">
+            <span class="cart-badge">3</span> <!-- Etiqueta para la cantidad de artículos -->
+        </div>
+    </div>
+</header>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const userPanel = document.getElementById('userPanel');
+        const userMenu = document.getElementById('userMenu');
+        const shoppingCart = document.getElementById('shoppingCart');
+        const categoryButton = document.getElementById('categoryButton');
+        const categoryMenu = document.getElementById('categoryMenu');
+
+        userPanel.addEventListener('click', function() {
+            userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
+        });
+
+        categoryButton.addEventListener('click', function() {
+            categoryMenu.style.display = categoryMenu.style.display === 'block' ? 'none' : 'block';
+        });
+
+        document.addEventListener('click', function(event) {
+            if (!userPanel.contains(event.target) && !userMenu.contains(event.target)) {
+                userMenu.style.display = 'none';
+            }
+            if (!categoryButton.contains(event.target) && !categoryMenu.contains(event.target)) {
+                categoryMenu.style.display = 'none';
+            }
+        });
+
+        shoppingCart.addEventListener('click', function() {
+            window.location.href = 'ruta/a/tu/pagina/del/carrito.html';
+        });
+    });
+
+</script>
+
+
+
+
+</body>
+</html>
